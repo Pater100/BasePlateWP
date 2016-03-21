0a319dbda04405fc2fce3bf327689463e4a42a10
Levdbasgit config --global user.name Levdbasgit config --global user.name Levdbas <erik@basedonline.nl>
2016-03-21 19:57:47 +0100

    Header verder opgedeel in twee files, oude files verwijderd.

--- templates/head.php
+++ templates/head.php
@@ -8,9 +8,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
         <?php wp_head(); ?>
         <?php include_once(dirname(__FILE__) . '/analyticstracking.php'); ?>
         <?php $pagetitle = strtolower(get_the_title()); ?>
